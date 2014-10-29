2aa74d04f14d9f3f92121fd959ba440c944f2cac
camaran <camaran@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-29 11:32:46 +0000

    New http-bind server
    git-svn-id: https://plugins.svn.wordpress.org/chatmeim-mini-messenger/trunk@1015873 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- chatmeim-mini-messenger.php
+++ chatmeim-mini-messenger.php
@@ -1,9 +1,9 @@
-﻿<?php
+<?php
 /*
 Plugin Name: ChatMe Mini Messenger
 Plugin URI: http://www.chatme.im/
 Description: This plugin add the javascript code for Chatme.im Mini Messenger a Jabber/XMPP chat for your WordPress.
-Version: 3.3.9
+Version: 3.3.10
 Author: camaran
 Author URI: http://www.chatme.im
 */
@@ -30,7 +30,7 @@ function get_chatme_messenger_head() {
 function get_chatme_messenger_footer() {
 
 	$lng = (get_option('language') == '') ? "en" : get_option('language');
-	$url = (get_option('hosted') == '1') ? "http://api.webchat.domains/http-bind/" : "http://api.chatme.im/http-bind/";
+	$url = (get_option('hosted') == '1') ? "http://webchat.domains/http-bind/" : "http://webchat.chatme.im/http-bind/";
 
 	//if(get_option('language') == '')
 	//	$lng = "en";
@@ -76,7 +76,7 @@ function mini_messenger_options() {
  <div class="wrap">
 <h2>ChatMe Mini Messenger</h2>
 <p><?php _e("For more information visit <a href='http://www.chatme.im' target='_blank'>www.chatme.im</a>", 'chatmeim-mini-messenger'); ?> - <a href="https://webchat.chatme.im/?r=support" target="_blank">Support Chat Room</a></p>
-<p><?php _e("For subscribe your account visit <a href='http://api.chatme.im/register_web' target='_blank'>http://api.chatme.im/register_web</a>", 'chatmini'); ?></p> 
+<p><?php _e("For subscribe your account visit <a href='http://webchat.chatme.im/register_web' target='_blank'>http://webchat.chatme.im/register_web</a>", 'chatmini'); ?></p> 
 
 <form method="post" action="options.php">
     <?php settings_fields( 'messenger_chat_msn' ); ?>
@@ -88,12 +88,12 @@ function mini_messenger_options() {
         	<select id="language" name="language">
         		<option value="de" <?php selected('de', get_option('language')); ?>>Deutsch</option>
         		<option value="en" <?php selected('en', get_option('language')); ?>>English</option>
-        		<option value="es" <?php selected('es', get_option('language')); ?>>Español</option>
-        		<option value="fr" <?php selected('fr', get_option('language')); ?>>Français</option>
+        		<option value="es" <?php selected('es', get_option('language')); ?>>Espa�ol</option>
+        		<option value="fr" <?php selected('fr', get_option('language')); ?>>Fran�ais</option>
         		<option value="it" <?php selected('it', get_option('language')); ?>>Italiano</option>
-        		<option value="ja" <?php selected('ja', get_option('language')); ?>>日本語</option>
+        		<option value="ja" <?php selected('ja', get_option('language')); ?>>???</option>
         		<option value="nl" <?php selected('nl', get_option('language')); ?>>Nederlands</option>
-        		<option value="ru" <?php selected('ru', get_option('language')); ?>>Русский</option>
+        		<option value="ru" <?php selected('ru', get_option('language')); ?>>???????</option>
         		</select><br />
                 <input type="checkbox" value="1" name="hosted" <?php if (get_option('hosted')=="1") { echo 'checked=""'; } ?> /> <?php _e("Hosted Domain", 'chatmeim-mini-messenger'); ?><br /><?php _e("For Active an hosted domain with XMPP service <a href='http://chatme.im' target='_blank'>visit www.chatme.im</>", 'chatmeim-mini-messenger'); ?>
         </td>
@@ -110,7 +110,7 @@ function mini_messenger_options() {
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="8CTUY8YDK5SEL">
-<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal � The safer, easier way to pay online.">
 <img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
 </form>
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.chatme.im
 Tags: chat, jabber, jappix, mini, xmpp, chatme
 Requires at least: 3.8
 Tested up to: 4.0
-Stable tag: 3.3.9
+Stable tag: 3.3.10
 
 This plugin add the javascript code for ChatMe Mini Messenger a Jabber/XMPP chat for your WordPress.
 
@@ -58,6 +58,9 @@ Yes!!! it's a Jabber/XMPP server, you see [here](http://chatme.im/prima-connessi
 2. The chat admin panel
 
 == Changelog ==
+
+= 3.3.10 =
+* New http-bind server
 
 = 3.3.9 =
 * ConverseJS [0.8.3](https://github.com/jcbrand/converse.js/releases/tag/v0.8.3) e [0.8.2](https://github.com/jcbrand/converse.js/releases/tag/v0.8.2)
@@ -157,6 +160,9 @@ Yes!!! it's a Jabber/XMPP server, you see [here](http://chatme.im/prima-connessi
 * First public version
 
 == Upgrade Notice ==
+
+= 3.3.10 =
+* New http-bind server
 
 = 3.3.9 =
 * ConverseJS [0.8.3](https://github.com/jcbrand/converse.js/releases/tag/v0.8.3) e [0.8.2](https://github.com/jcbrand/converse.js/releases/tag/v0.8.2)
